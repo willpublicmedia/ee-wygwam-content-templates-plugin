e1b2452d168763626b91f5b04a94ab6af1bb0a91
Jack Brighton <jackbrighton@gmail.com>
2015-06-25 15:26:34 -0500

    adjust placeholder images and js

--- wygwam-content-templates/wygwam-content-templates1.js
+++ wygwam-content-templates/wygwam-content-templates1.js
@@ -15,9 +15,9 @@
  	 		html:'<figure class="pull-right"><img src="https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div</figure>'},
  	 		{
  	 		title:"Full-width with caption",
- 	 		image:"captioned-image-800.png",
+ 	 		image:"captioned-image-800.gif",
  	 		description:"Add a full-sized image (800px wide) with a caption below it.",
- 	 		html:'<figure class="full-image"><img src="https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div</figure>'}, 	 		
+ 	 		html:'<figure class="full-image"><img src="https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image-800.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div</figure>'}, 	 		
  	 		{
  	 		title:"Pullquote",
  	 		image:"",
