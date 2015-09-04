10137976d09f86cd5cbd33b283709ef8da873ac8
Jack Brighton <jackbrighton@gmail.com>
2015-09-04 14:58:53 -0500

    fix a few errors

--- wygwam-content-templates/wygwam-content-templates1.js
+++ wygwam-content-templates/wygwam-content-templates1.js
@@ -2,25 +2,25 @@
  Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
  */
- CKEDITOR.addTemplates("default",{imagesPath:CKEDITOR.getUrl("https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/"),
+ CKEDITOR.addTemplates("default",{imagesPath:CKEDITOR.getUrl("//will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/"),
  	 	templates:[{
  	 		title:"Image on the left with caption",
  	 		image:"captioned-image-left.gif",
  	 		description:"Add an image on the left with a caption below it.",
- 	 		html:'<figure class="pull-left image-template"><img src="https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div></figure>'},
+ 	 		html:'<figure class="pull-left image-template"><img src="//will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div></figure>'},
  	 		{
  	 		title:"Image on the right with caption",
  	 		image:"captioned-image-right.gif",
  	 		description:"Add an image on the right with a caption below it.",
- 	 		html:'<figure class="pull-right image-template"><img src="https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div</figure>'},
+ 	 		html:'<figure class="pull-right image-template"><img src="//will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div></figure>'},
  	 		{
  	 		title:"Full-width with caption",
- 	 		image:"captioned-image-800.gif",
+ 	 		image:"captioned-image-full.png",
  	 		description:"Add a full-sized image (800px wide) with a caption below it.",
- 	 		html:'<figure class="full-image"><img src="https://will-dev.will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image-800.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div</figure>'}, 	 		
+ 	 		html:'<figure class="full-image"><img src="//will.illinois.edu/themes/site_themes/will2.1/plugins/wygwam-content-templates/placeholder-image-800.png" alt=""><figcaption><p>Replace this text with your caption</p></figcaption><div class="photocredit">Photo Credit: </div></figure>'}, 	 		
  	 		{
  	 		title:"Pullquote",
  	 		image:"",
  	 		description:"",
- 	 		html:'<div class="pullquote"><blockquote>Maybe replace this with something more interesting than this sentence?<cite>Wise Man</cite></blockquote</div>'}
+ 	 		html:'<div class="pullquote"><blockquote>Maybe replace this with something more interesting than this sentence?<cite>Wise Man</cite></blockquote></div>'}
  	 		]});
